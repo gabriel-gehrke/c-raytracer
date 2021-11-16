cb6c071f7fa511332bb7229f3e24a240f557a1dd
BuildTools <unconfigured@null.spigotmc.org>
2021-11-16 20:44:01 +0100

    added util.h file

---
+++ util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string.h>
+
+// checks if two variables/structs are equal by comparing their size in bytes and performing a memcmp
+#define EQUALS(A, B) ((sizeof(A) == sizeof(B)) && (memcmp(&A, &B, sizeof(A)) == 0))
+
+#endif
